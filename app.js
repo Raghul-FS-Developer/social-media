6c727f056be58e682af002a529413922dbebf8de
princeraghul <raghulraghu2002@gmail.com>
2022-06-28 18:35:50 +0530

    final commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static("uploads"))
-app.use(cors())
+app.use(cors());
 app.use(helmet())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -33,7 +33,8 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
